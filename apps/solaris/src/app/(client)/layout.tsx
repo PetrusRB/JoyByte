67a28649283b6de4cf025d703e6e673984dd8a57
PetrusRB <pedro.co.dev.ac@gmail.com>
2025-06-18 20:34:16 -0400

    Fix some errors on layout.tsx(client), and remove ErrorBoundary.

--- apps/solaris/src/app/(client)/layout.tsx
+++ apps/solaris/src/app/(client)/layout.tsx
@@ -7,7 +7,6 @@ import { ThemeProvider } from 'next-themes'
 import Navbar from "@/components/Navbar";
 import MobileNav from "@/components/MobileNav";
 import React from "react";
-import ErrorBoundary from "@/components/ErrorBoundary";
 import { Toaster } from "@/components/ui/Toast";
 
 type LayoutClientProps = {
@@ -19,19 +18,17 @@ const LayoutClient: React.FC<LayoutClientProps> = ({ children }) => {
     <AntdRegistry>
       <ThemeProvider attribute='class' defaultTheme="system">
         <AuthProvider>
-          <ErrorBoundary>
-            {/* Navbar sempre visível, dentro do AuthContext */}
-            <Navbar />
+          {/* Navbar sempre visível, dentro do AuthContext */}
+          <Navbar />
 
-            {/* Aqui irá renderizar o layout com home/login */}
-            <div className="flex-1">
+          {/* Aqui irá renderizar o layout com home/login */}
+          <div className="flex-1">
               {children}
-            </div>
-            {/* Notificações */}
-            <Toaster/>
-            {/* Navegação mobile fixa embaixo */}
-            <MobileNav />
-          </ErrorBoundary>
+          </div>
+          {/* Notificações */}
+          <Toaster/>
+          {/* Navegação mobile fixa embaixo */}
+          <MobileNav />
         </AuthProvider>
       </ThemeProvider>
     </AntdRegistry>
